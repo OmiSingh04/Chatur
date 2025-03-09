654bb2ff56123880c51baf1341225eaa2f4cc530
OmiSingh04 <omisinghdbz@gmail.com>
2025-03-09 16:30:11 +0530

    timing

--- src/main/java/TimedWordGet.java
+++ src/main/java/TimedWordGet.java
@@ -1,5 +1,26 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+import java.awt.*;
+
 public class TimedWordGet {
 
+    public static void sendDailyWord(JDA jda){
 
+        String word = WordFetch.fetchWord();
+
+        TextChannel channel = jda.getTextChannelById(Main.channelId);
+        channel.sendMessageEmbeds(
+                new EmbedBuilder()
+                        .setTitle("Word of the Day!")
+                        .setDescription("Your daily word. Learn about it and use it in a sentence.")
+                        .setColor(Color.CYAN)
+                        .addField("Word", word, true).build()
+        ).queue();
+
+    }
 
 }
